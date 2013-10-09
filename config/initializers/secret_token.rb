b1b2358c991a47c9b9e9f2bf6b1c4f747f80e78e
Bernie Chiu <bernie_chiu@hotmail.com>
2013-10-09 18:55:57 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsboys::Application.config.secret_token = 'ca403a83a2b605640a5e8793733c07dec308dc8f8140ceab8cfb52b2ed28951459ac38ea6a7b6da981c1e02499a2e721293e4756550a01513cb25f00eec8a41e'
